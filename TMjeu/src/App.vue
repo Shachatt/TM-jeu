fa217105c840589165bbdfc8a2ba5a15a2193837
Shachatt <ps37twh@eduvaud.ch>
2026-08-31 23:26:00 +0200

    début fonction de comparaison
    Added functions for grid navigation and value checking.

--- TMjeu/src/App.vue
+++ TMjeu/src/App.vue
@@ -9,12 +9,18 @@ function GrilleVide() {
         { valeur: null, verrouille: false}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false}
     ]
     }
+const grille = GrilleVide() //on a notre grille mtn
 //fonction pour se repérer dans la grille
 function ligne(i:number) { 
     return Math.floor(i/4)
 }
 function colonne(i:number) {
     return i % 4
+}
+//fonction pour contrôler s'il y même valeur sur la ligne / colonne
+function memeligne(){
+    for
+    grille[i]
 }
 </script>
 
