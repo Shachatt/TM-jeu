da3daeb45ea2c1174c3f743b7e5be93bc9556a65
Shachatt <ps37twh@eduvaud.ch>
2026-09-01 16:25:01 +0200

    Update Tailwind CSS import in Vite config

--- TMjeu/vite.config.ts
+++ TMjeu/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(),
-            tailwindcss()
-  ],
+            tailwindcss() as any],
 })
