4b61fcc6ee2295a4998b7868b8524004aa92b456
Shachatt <ps37twh@eduvaud.ch>
2026-09-01 13:21:51 +0200

    fonction même ligne/colonne pas fini

--- TMjeu/src/App.vue
+++ TMjeu/src/App.vue
@@ -17,11 +17,25 @@ function ligne(i:number) {
 function colonne(i:number) {
     return i % 4
 }
-//fonction pour contrôler s'il y même valeur sur la ligne / colonne
-function memeligne(){
-    for
-    grille[i]
+//fonction pour contrôler s'il y même valeur sur la colonne / ligne
+
+function memeColonne(grille, index, let valeur:number){
+    for (let i=0 ; i < 16 ; i += 4) { // comment faire pour dire que i c'est l'élé qu'on a choisi ?
+        if ( valeur === grille[i.valeur]) {
+            return false
+        }
+    }
+    return true
 }
+function memeLigne(grille, index, let valeur:number){
+    for (let  i = 0; i < 16 ; i++) {
+        if ( valeur === grille[i.valeur]){
+            return false
+        }
+    }
+    return true
+}
+
 </script>
 
 <template>
