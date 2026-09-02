3c2830736cb4c033ff783c717ab08db181981f56
Shachatt <ps37twh@eduvaud.ch>
2026-09-02 07:51:30 +0200

    faire les tests des fonctions

--- TMjeu/src/App.vue
+++ TMjeu/src/App.vue
@@ -10,6 +10,7 @@ function GrilleVide() {
     ]
     }
 const grille = GrilleVide() //on a notre grille mtn
+type Casevide = {valeur:number|null, verrouille:boolean}
 //fonction pour se repérer dans la grille
 function ligne(i:number) { 
     return Math.floor(i/4)
@@ -18,29 +19,33 @@ function colonne(i:number) {
     return i % 4
 }
 //fonction pour contrôler s'il y même valeur sur la colonne / ligne
-
-function pasmemeColonne(grille:{valeur:number|null;verrouille:boolean}[],index:number, valeur:number){
-    for (let i=colonne(index) ; i < 16 ; i += 4) { // comment faire pour dire que i c'est l'élé qu'on a choisi ?
-        if ( valeur === grille[i].valeur) {
+function pasmemeColonne(grille:Casevide[],index:number, valeur:number){
+    for (let i=colonne(index) ; i < 16 ; i += 4) { 
+        if ( valeur === grille[i]?.valeur) { //grille[i].valeur : prend la valeur de ce qu'il y a à cet index : ?. permet d'éviter l'undefind
             return false
         }
     }
     return true
 }
-function pasmemeLigne(grille:{valeur:number|null;verrouille:boolean}[],index:number, valeur:number){
+function pasmemeLigne(grille:Casevide[],index:number, valeur:number){
     for (let  i = ligne(index)*4 ; i < ligne(index)*4+4 ; i++) {
-        if ( valeur === grille[i].valeur){
+        if ( valeur === grille[i]?.valeur){
             return false
         }
     }
     return true
 }
-function peutPlacer(grille:{valeur:number;verrouille:boolean}[],index:number, valeur:number){
+function peutPlacer(grille:Casevide[],index:number, valeur:number){
     return pasmemeColonne(grille,index,valeur) && pasmemeLigne(grille,index,valeur) 
 }
+const testgrille = [{ valeur: null, verrouille: false}, { valeur: 2, verrouille: true}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false},
+        { valeur: null, verrouille: false}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false},
+        { valeur: 3, verrouille: true}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false}, { valeur: 2, verrouille: true},
+        { valeur: null, verrouille: false}, { valeur: 1, verrouille: true}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false}
+    ]
 
+console.log(peutPlacer(testgrille, 9, 4))
 </script>
-
 <template>
 </template>
 
