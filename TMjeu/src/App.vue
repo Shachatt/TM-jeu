d40829ffe401cea6fddf54eaeab2c2bf2d6cb709
Shachatt <ps37twh@eduvaud.ch>
2026-09-12 11:03:44 +0200

    fonction + test de génération de grille

--- TMjeu/src/App.vue
+++ TMjeu/src/App.vue
@@ -39,24 +39,24 @@ function peutPlacer(grille:Casevide[],index:number, valeur:number){
     return pasmemeColonne(grille,index,valeur) && pasmemeLigne(grille,index,valeur) 
 }
 
-function melanger(tableau: number[]): number[] { // pourquoi il y a deux fois number[]
-    const restants = [...tableau]   //je n'ai as compris pour tableau est écrit entre [] et avec les trois points devant
+function melanger(tableau: number[]): number[] { // number dedans : le type dans le tableau, ext ce qui va en sortir
+    const restants = [...tableau]   //le spread(...) consiste à étaler son contenu, ça crée une copie (car sinon c'est dans même case de stockage)
     const resultat: number[] = [] //si il y a : number[] c'est pour préciser que le résultat sera seulement un nombre ? et les deux [] pour dire que la case est actuellement vide ?
     while (restants.length > 0) { // le tableau des restants se vide, donc on continue de faire tant qu'il y aqlch dans le tableau
-        const i = Math.floor(Math.random() * restants.length) // a quel moment ça sait que c'est des chiffres entre 1 et 4 ?, et je ne comprends pas vrmt le calcul, il y a quoi dans le math random ?
-        resultat.push(...restants.splice(i, 1))//peux-tu me réexpliquer push et les 3 points
-    }
+        const i = Math.floor(Math.random() * restants.length) //random trouve un décimal entre 0 et(*) la longueur restante, floor c'est pour arrondir vers la bas, 4-3-2-1 
+        resultat.push(...restants.splice(i, 1))//push ajoute des éléments à la fin d'un tableau, splice enleve éé et return un tableau[] et les point servent à envoyer un number
+    } //fonction générique de mélange de tableau
     return resultat //car c'est ce qu'on a besoin
 }
 
-function placerChiffre(index: number): boolean { // pourquoi le :boolean n'est pas dans la parenthese ?
-    if (index >= 16) { //vérifie que les 15 cases sont remplis, si oui on arrête tout
+function placerChiffre(index: number): boolean { // on rentre un chiffre et la fonction nous rend un boolean
+    if (index >= 16) { //vérifie que les 16 (0 à 15) cases sont remplis, si oui on arrête tout
         return true
     }
     const caseCourante = grille[index] //c'est las case actuelle avec l'index dune case de notre grille
-    if (!caseCourante) return false //vérifie si case existe, ! = si existe, et si ça existe pas c'est faux, donc ça vérifie pour la suite
-    const candidats = melanger([1, 2, 3, 4])//ici va utiliser la fonction pour recréer un tableau avec 1 2 3 et 4 mélangé. mais ça en sort qu'un élément ?
-    for (const candidat of candidats) { //c'est quoi for (_ of _) ? fin le return fini la boucle ? jsp j'ai pas trop compris
+    if (!caseCourante) return false //vérifie si case existe, !qlch = si qlch est faux on return false, donc si ça existe pas, donc ça vérifie pour la suite
+    const candidats = melanger([1, 2, 3, 4])//ici va utiliser la fonction pour recréer un tableau avec 1 2 3 et 4 mélangé. ça sort un tableau
+    for (const candidat of candidats) { //for of est une bucle qui parcourt les valeurs du tableau, sans passer par index : "pour chacun des chiffres de la liste"
         if (peutPlacer(grille, index, candidat)) {
             caseCourante.valeur = candidat // if true, alors on peut écrire la valeur dans la case
             if (placerChiffre(index + 1)) { // si la case d'après renvoie aussi true alors c'est nice. RECURSIVITE la fonction se relance pour les case suivantes
@@ -68,20 +68,7 @@ function placerChiffre(index: number): boolean { // pourquoi le :boolean n'est p
 
     return false //si les quatres on échoué valeur on échoué on retourne false après avoir effacé la valeur de la case
 }
-//function placerChiffreancien(i:number){
-  //  let chiffres = sort.[1;2;3;4]
- //   if (i === 16){  //si tous les index sont passés alors case remplies et juste
-   //     return true
-   // } else {
-//        const val = grille[i]
-  //          if (!val) return false
-    //        grille.valeur = val //je ne comprend pas vraiment cette ligne
- //           return false
-   //     } 
-   // for n of grille[] {
-//    }
-//}
-    
+   
 const testgrille = [{ valeur: null, verrouille: false}, { valeur: 2, verrouille: true}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false},
         { valeur: null, verrouille: false}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false},
         { valeur: 3, verrouille: true}, { valeur: null, verrouille: false}, { valeur: null, verrouille: false}, { valeur: 2, verrouille: true},
@@ -92,6 +79,7 @@ console.log(peutPlacer(testgrille, 9, 4))
 console.log(peutPlacer(testgrille, 9, 3))
 console.log(peutPlacer(testgrille, 9, 1))
 console.log(reussi, grille)
+console.log(melanger([1,2,3,4]))
 
 </script>
 <template>
